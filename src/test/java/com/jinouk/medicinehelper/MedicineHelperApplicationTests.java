61e089e6548722878712e5dc481d8e243a81497f
LimJinOuk <jinouk038@gmail.com>
2025-05-07 00:31:28 +0900

    first commit

---
+++ src/test/java/com/jinouk/medicinehelper/MedicineHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinouk.medicinehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
